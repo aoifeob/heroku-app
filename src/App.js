7f16061a6da50d250787a31fcae5e963b76daca4
obriea21 <obriea21@tcd.ie>
2016-11-18 16:22:46 +0000

    where are the cats?

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.</p>
-<p className="App-intro" size="100">        We're up and running.
-        </p>
+<p className="App-intro" size="100"> I was told the purpose of the internet is to look at cat pictures.  Why can't I see cat pictures? </p>
       </div>
     );
   }
