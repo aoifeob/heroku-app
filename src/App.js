27b37431bde7ccc491d62e2f0a90f16dc3f410c6
obriea21 <obriea21@tcd.ie>
2016-11-18 18:10:06 +0000

    cat central

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Cat Central</h2>
         </div>
 <p> I was told the purpose of the internet is to look at cat pictures.  Why can't I see cat pictures? </p>
 
