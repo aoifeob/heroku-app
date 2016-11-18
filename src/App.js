feae2334bc779924c22a300ebfe4c878a0ea0127
obriea21 <obriea21@tcd.ie>
2016-11-18 18:43:23 +0000

    fewer cats

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Cat Central</h2>
+        </div>
+<p> I was told the purpose of the internet is to look at cat pictures.  Why can't I see cat pictures? </p>
+
+<p> The following is my definitive list of Best Internet Cats:</p>
+<p><ol><li>Grumpy Cat</li><li>British Treasury Cat</li><li>10 Downing Street Cat</li><li>Garfield</li></ol></p>
+<p> I love cats.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './js/App';
-import './css/index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <App />,
