6b2606337f4372f7595623fc447f8282eb0db54c
obriea21 <obriea21@tcd.ie>
2016-11-18 16:25:45 +0000

    still no cats

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.</p>
 <p className="App-intro" size="100"> I was told the purpose of the internet is to look at cat pictures.  Why can't I see cat pictures? </p>
       </div>
     );
